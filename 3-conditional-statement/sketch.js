5b46c1d272d0ffb388ae9ed1a3fc58315ee33630
lucylp <56699398+lucylp@users.noreply.github.com>
2019-11-20 14:43:15 -0800

    Deleted ball class

--- 3-conditional-statement/sketch.js
+++ 3-conditional-statement/sketch.js
@@ -11,19 +11,20 @@ function setup() {
 
   //make one avatar called me
   me = new Avatar(width/2, 300, 4);
-  bar = new Bar(-100, 1);
+  bar = new Bar(-160, 1);
 
 }
 
 function draw(){
 	background(92, 188, 214);
 
+
   bar.drawBar();
   bar.moveBar();
 
 
   if (frameCount % 70 == 0) {
-      let  b = new Bar(-100, 1);
+      let  b = new Bar(-160, 1);
       bars.push(b);
       console.log(bars); //print the balls array to the console
     }
@@ -41,13 +42,13 @@ function draw(){
 
 function keyPressed() {
      if (keyCode === UP_ARROW) {
-       me.y = me.y - 50;
+       me.y = me.y - 70;
      }
      else if (keyCode === RIGHT_ARROW) {
-       me.x += 50;
+       me.x += 70;
      }
      else if (keyCode === LEFT_ARROW) {
-       me.x = me.x - 50;
+       me.x = me.x - 70;
      }
 
      return false; // prevent default
@@ -77,39 +78,6 @@ class Avatar {
 
 }
 
-//ball class from which to create new balls with similar properties.
-class Ball {
-
-	//every ball needs an x value, a y value, and a speed
-	constructor(x,y, speed){
-		this.x = x;
-    this.y = y;
-    this.speed = speed;
-	}
-
-	// draw a ball on the screen at x,y
-	drawBall(){
-    	stroke(0);
-      strokeWeight(0);
-    	fill("blue");
-		  ellipse(this.x,this.y,10,10);
-	}
-
-	//update the location of the ball, so it moves across the screen
-	moveBall(){
-		this.x = this.x;
-		this.y = this.y-this.speed;
-	}
-
-	//if the ball hits the person, change the speed value to negative (send it in the opposite direction)
-  	bounceBall(){
-    		if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-40 && this.y < me.y+40){
-      			this.speed = -this.speed;
-    		}
-  	}
-
-}
-
 class Bar {
   //These are the background!!!!!
   //every bar needs a speed of descent (y axis), and a y value for a change in position
@@ -120,8 +88,11 @@ class Bar {
 
   //draw a bar on the screen at (0,0)
   drawBar(){
-    fill("green");
-    rect(0, this.y, 500, 70);
+    stroke("black");
+    strokeWeight(3);
+    fill("white");
+    rect(0, this.y, 550, 70);
+
   }
 
   //update the location of the bar, so it moves down the screen
